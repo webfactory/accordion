09f1cd91f743b2567d4ac62fe0bece1343488705
Søren Birkemeyer <sb@webfactory.de>
2019-07-17 14:28:30 +0200

    Rewrite plugin to avoid multiple instantiations
    Follow the pattern outlined in https://github.com/jquery-boilerplate/jquery-patterns/blob/master/patterns/jquery.highly-configurable.plugin.boilerplate.js
    to prevent multiple instantiations and other funky side effects like "it works although I haven't even added the correct class attributes (JS hooks) in my example.html".

--- wf.accordion.js
+++ wf.accordion.js
@@ -1,162 +1,185 @@
-(function($) {
+;(function($, window, document, undefined) {
     'use strict';
 
     /**
      * Create a counter (used a for creation of unique IDs)
      * @type {number}
      */
-    var counter = 0;
+    window.wfaccordion = window.wfaccordion || {};
+    window.wfaccordion.counter = window.wfaccordion.counter || 0;
 
-    /**
-     * wfAccordion plugin definition
-     * @param {Object} [options]
-     */
-    $.fn.wfAccordion = function(options) {
+    // constructor
+    var Accordion = function(elem, options) {
+        this.elem = elem;
+        this.$elem = $(elem);
+        this.options = options;
+    };
 
-        var defaults = {
+    // the plugin prototype
+    Accordion.prototype = {
+        defaults: {
             context: 'body',
             accordionHeader: '.js-accordion__header',
             accordionTrigger: '.js-accordion__trigger',
             accordionPanel: '.js-accordion__panel',
             accordionIsOpenClass: 'js-accordion--is-expanded'
-        };
+        },
 
-        // Create settings from defaults and options
-        var settings = $.extend({}, defaults, options);
+        init: function() {
+            // Introduce defaults that can be extended either
+            // globally or using an object literal.
+            this.settings = $.extend({}, this.defaults, this.options);
 
-        var $context = $(settings.context);
-        var $accordionHeaders = $context.find(settings.accordionHeader);
+            var $context = $(this.settings.context);
+            var $accordionHeaders = $context.find(this.settings.accordionHeader);
+            var self = this;
 
-        $accordionHeaders.each(function(index, header) {
-            var $header = $(header);
-            var $triggerPlaceholder = $header.find(settings.accordionTrigger);
-            var $panel = $header.next(settings.accordionPanel);
+            $accordionHeaders.each(function(index, header) {
+                var $header = $(header);
+                var $triggerPlaceholder = $header.find(self.settings.accordionTrigger);
+                var $panel = $header.next(self.settings.accordionPanel);
 
-            var $trigger = enhanceWithButton($triggerPlaceholder);
+                var $trigger = self.enhanceWithButton($triggerPlaceholder);
 
-            // Enhance triggers and panels with ARIA attributes
-            $trigger.attr('aria-expanded', false);
-            $panel.attr('aria-hidden', true);
+                // Enhance triggers and panels with ARIA attributes
+                $trigger.attr('aria-expanded', false);
+                $panel.attr('aria-hidden', true);
 
-            // Create unique IDs for use in ARIA relationships
-            var headerId = 'accordion-' + counter + '__header-' + index;
-            var panelId = 'accordion-' + counter +  '__panel-' + index;
+                // Create unique IDs for use in ARIA relationships
+                var headerId = 'accordion-' + window.wfaccordion.counter + '__header-' + index;
+                var panelId = 'accordion-' + window.wfaccordion.counter +  '__panel-' + index;
 
-            // Create ARIA relationships between headers and panels
-            $trigger.attr('aria-controls', panelId);
-            $panel.attr('id', panelId);
+                // Create ARIA relationships between headers and panels
+                $trigger.attr('aria-controls', panelId);
+                $panel.attr('id', panelId);
 
-            $trigger.attr('id', headerId);
-            $panel.attr('aria-labelledby', headerId);
+                $trigger.attr('id', headerId);
+                $panel.attr('aria-labelledby', headerId);
 
-            // Open accordions that were set to "open on startup" via class
-            if ($header.parent().hasClass(settings.accordionIsOpenClass)) {
-                $trigger.attr('aria-expanded', true);
-                $panel.attr('aria-hidden', false)
+                // Open accordions that were set to "open on startup" via class
+                if ($header.parent().hasClass(self.settings.accordionIsOpenClass)) {
+                    $trigger.attr('aria-expanded', true);
+                    $panel.attr('aria-hidden', false)
+                }
+
+                // Update ARIA states on click/tap
+                $trigger.on('click', function(event) {
+                    var $target = $(event.target);
+                    var state = $target.attr('aria-expanded') === 'false';
+
+                    $target.attr('aria-expanded', state);
+                    $('#' + $target.attr('aria-controls')).attr('aria-hidden', !state);
+                });
+
+                // Enable keyboard support
+                $trigger.on('keydown', function(event) {
+                    var $target = $(event.target);
+                    var key = event.which.toString();
+                    var $header = $target.parents(self.settings.accordionHeader);
+
+                    // 33 = Page Up, 34 = Page Down
+                    var ctrlModifier = (event.ctrlKey && key.match(/33|34/));
+
+                    // Support up / down arrows as well as Ctrl + Page Up / Page Down keys
+                    // 38 = Up, 40 = Down
+                    if (key.match(/38|40/) || ctrlModifier) {
+                        var index = $accordionHeaders.index($header);
+                        var direction = (key.match(/34|40/)) ? 1 : -1;
+                        var length = $accordionHeaders.length;
+                        var newIndex = (index + length + direction) % length;
+
+                        $($accordionHeaders[newIndex]).find(self.settings.accordionTrigger).focus();
+
+                        event.preventDefault();
+                    } else if (key.match(/35|36/)) {
+                        // Support End and Home keys
+                        // 35 = End, 36 = Home
+                        switch (key) {
+                            // Focus the first accordion
+                            case '36':
+                                $($accordionHeaders[0]).find(self.settings.accordionTrigger).focus();
+                                break;
+                            // Focus the last accordion
+                            case '35':
+                                $($accordionHeaders[$accordionHeaders.length - 1]).find(self.settings.accordionTrigger).focus();
+                                break;
+                        }
+
+                        event.preventDefault();
+                    }
+                });
+            });
+
+            // Increment the counter with every instantiation of the plugin
+            window.wfaccordion.counter++;
+
+            return this;
+        },
+
+        sampleMethod: function() {
+            // eg. show the currently configured message
+            console.log(this.config.message);
+        },
+
+        /**
+         * Enhance a given placeholder element with a <button> for better keyboard support
+         *
+         * @param {jQuery} $placeholder
+         * @returns {jQuery}
+         */
+        enhanceWithButton: function($placeholder) {
+            // Support the case that the placeholder may already be a button
+            if ($placeholder.get('0').nodeName && $placeholder.get('0').nodeName.toLowerCase() === 'button') {
+                return $placeholder;
             }
 
-            // Update ARIA states on click/tap
-            $trigger.on('click', function(event) {
-                var $target = $(event.target);
-                var state = $target.attr('aria-expanded') === 'false';
+            var $button = $('<button>' + $placeholder.text() + '</button>');
 
-                $target.attr('aria-expanded', state);
-                $('#' + $target.attr('aria-controls')).attr('aria-hidden', !state);
+            $.each(this.getAttributes($placeholder), function(key, value) {
+                $button.attr(key, value);
             });
 
-            // Enable keyboard support
-            $trigger.on('keydown', function(event) {
-                var $target = $(event.target);
-                var key = event.which.toString();
-                var $header = $target.parents(settings.accordionHeader);
+            $placeholder.html($button);
 
-                // 33 = Page Up, 34 = Page Down
-                var ctrlModifier = (event.ctrlKey && key.match(/33|34/));
+            this.removeAllAttributes($placeholder);
 
-                // Support up / down arrows as well as Ctrl + Page Up / Page Down keys
-                // 38 = Up, 40 = Down
-                if (key.match(/38|40/) || ctrlModifier) {
-                    var index = $accordionHeaders.index($header);
-                    var direction = (key.match(/34|40/)) ? 1 : -1;
-                    var length = $accordionHeaders.length;
-                    var newIndex = (index + length + direction) % length;
+            return $button;
+        },
 
-                    $($accordionHeaders[newIndex]).find(settings.accordionTrigger).focus();
+        /**
+         * Return all HTML attributes (and their values) of a given jQuery object
+         * @param {jQuery} $node
+         * @returns {{}}
+         */
+        getAttributes: function($node) {
+            var attrs = {};
+            $.each( $node[0].attributes, function (index, attribute) {
+                attrs[attribute.name] = attribute.value;
+            } );
 
-                    event.preventDefault();
-                } else if (key.match(/35|36/)) {
-                    // Support End and Home keys
-                    // 35 = End, 36 = Home
-                    switch (key) {
-                        // Focus the first accordion
-                        case '36':
-                            $($accordionHeaders[0]).find(settings.accordionTrigger).focus();
-                            break;
-                        // Focus the last accordion
-                        case '35':
-                            $($accordionHeaders[$accordionHeaders.length - 1]).find(settings.accordionTrigger).focus();
-                            break;
-                    }
+            return attrs;
+        },
 
-                    event.preventDefault();
-                }
-            });
-        });
+        /**
+         * Strip all HTML attributes (and their values) from a given jQuery object
+         * @param {jQuery} $node
+         */
+        removeAllAttributes: function($node) {
+            // Support non-jQuery node objects, just in case…
+            var elem = $node instanceof jQuery ? $node.get('0') : $node;
 
-        // Increment the counter with every call of the plugin
-        counter++;
+            while(elem.attributes.length > 0) {
+                elem.removeAttribute(elem.attributes[0].name);
+            }
+        }
     };
 
-    /**
-     * Enhance a given placeholder element with a <button> for better keyboard support
-     *
-     * @param {jQuery} $placeholder
-     * @returns {jQuery}
-     */
-    function enhanceWithButton($placeholder) {
-        // Support the case that the placeholder may already be a button
-        if ($placeholder.get('0').nodeName && $placeholder.get('0').nodeName.toLowerCase() === 'button') {
-            return $placeholder;
-        }
+    Accordion.defaults = Accordion.prototype.defaults;
 
-        var $button = $('<button>' + $placeholder.text() + '</button>');
-
-        $.each(getAttributes($placeholder), function(key, value) {
-            $button.attr(key, value);
+    $.fn.wfAccordion = function(options) {
+        return this.each(function() {
+            new Accordion(this, options).init();
         });
+    };
 
-        $placeholder.html($button);
-
-        removeAllAttributes($placeholder);
-
-        return $button;
-    }
-
-    /**
-     * Return all HTML attributes (and their values) of a given jQuery object
-     * @param {jQuery} $node
-     * @returns {{}}
-     */
-    function getAttributes($node) {
-        var attrs = {};
-        $.each( $node[0].attributes, function (index, attribute) {
-            attrs[attribute.name] = attribute.value;
-        } );
-
-        return attrs;
-    }
-
-    /**
-     * Strip all HTML attributes (and their values) from a given jQuery object
-     * @param {jQuery} $node
-     */
-    function removeAllAttributes($node) {
-        // Support non-jQuery node objects, just in case…
-        var elem = $node instanceof jQuery ? $node.get('0') : $node;
-
-        while(elem.attributes.length > 0) {
-            elem.removeAttribute(elem.attributes[0].name);
-        }
-    }
-
-})(jQuery);
+})(jQuery, window , document);
