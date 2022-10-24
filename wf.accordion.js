f9925d221f2b1d9844a58b68b23b06b4a99b7c85
Stefan Hilger <sh@webfactory.de>
2022-10-24 14:03:13 +0200

    Refactor Script

--- wf.accordion.js
+++ wf.accordion.js
@@ -1,293 +1,196 @@
-;(function($, window, document, undefined) {
-    'use strict';
+import {slugify, setUrlHash, triggerIdMatchesUrlHash, getAttributes, removeAllAttributes, removeUrlHash, getUrlHash} from "./helper";
 
-    /**
-     * Returns the loaction hash minus the hash-symbol
-     * @returns {string}
-     */
-    function getUrlHash() {
-        return window.location.hash.replace('#', '');
-    }
+/**
+ * Create a counter (used a for creation of unique IDs)
+ *
+ * @type {number}
+ */
+window.wfaccordion = window.wfaccordion || {};
+window.wfaccordion.groups = window.wfaccordion.groups || [];
+window.wfaccordion.slugs = window.wfaccordion.slugs || [];
 
-    /**
-     * Use history.replaceState so clicking through accordions
-     * does not create dozens of new history entries.
-     * Browser back should navigate to the previous page
-     * regardless of how many accordions were activated.
-     *
-     * @param {string} hash
-     */
-    function setUrlHash( hash ) {
-        if (history.replaceState) {
-            history.replaceState(null, '', '#' + hash);
+class wfaccordion {
+    constructor(elem, options) {
+        this.settings = {
+            ...{
+                accordionHeader: '.js-accordion__header',
+                accordionTrigger: '.js-accordion__trigger',
+                accordionPanel: '.js-accordion__panel',
+                disableHashUpdate: false
+            },
+            ...options
+        }
+        this.root = elem;
+        this.header = this.root.querySelector(this.settings.accordionHeader);
+        this.panel = this.root.querySelector(this.settings.accordionPanel);
+        this.placeholder = this.root.querySelector(this.settings.accordionTrigger);
+        this.slug = this.root.getAttribute('id') || slugify(this.placeholder.textContent);
+        this.slugOccurence = window.wfaccordion.slugs.filter((slug, index) => {
+            return slug.indexOf(this.slug) >= 0
+        }).length;
+
+        if (!window.wfaccordion.slugs.includes(this.slug)) {
+            window.wfaccordion.slugs.push(this.slug);
+            this.headerID = this.slug;
+            this.panelID = this.slug + '-panel';
         } else {
-            location.hash = hash;
+            window.wfaccordion.slugs.push(this.slug + '-' + this.slugOccurence);
+            this.headerID = this.slug + '-' + this.slugOccurence;
+            this.panelID = this.slug + '-panel' + '-' + this.slugOccurence;
         }
-    }
 
-    /**
-     * Use history.replaceState so clicking through accordions
-     * does not create dozens of new history entries.
-     * Browser back should navigate to the previous page
-     * regardless of how many accordions were activated.
-     */
-    function removeUrlHash() {
-        if (history.replaceState) {
-            history.replaceState(null, '', ' ');
+        this.enhanceWithButton();
+        this.events();
+
+        // Prevent duplicated IDs on root and header element
+        this.root.removeAttribute('id');
+
+        // Create ARIA relationships between headers and panels
+        this.trigger.setAttribute('aria-controls', this.panelID);
+        this.panel.setAttribute('id', this.panelID);
+
+        this.trigger.setAttribute('id', this.headerID);
+        this.panel.setAttribute('aria-labelledby', this.headerID);
+
+        // Get initial state
+        this.isDisabled = this.root.hasAttribute('data-wf-accordion-disabled');
+        this.isTargetOfUrlHash = triggerIdMatchesUrlHash(this.trigger);
+        this.isExpandedOnStartup = this.root.hasAttribute('data-wf-accordion-expanded') || this.isTargetOfUrlHash;
+
+        // Set initial state
+        this.trigger.setAttribute('aria-disabled', this.isDisabled);
+        this.trigger.setAttribute('aria-expanded', this.isExpandedOnStartup);
+        this.panel.setAttribute('aria-hidden', !this.isExpandedOnStartup);
+
+        if (this.isTargetOfUrlHash) {
+            this.trigger.focus()
         }
+
     }
 
-    /**
-     * Create a slug from any string.
-     * https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
-     *
-     * @type {string}
-     */
-    function slugify(string) {
-        var a = 'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;';
-        var b = 'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------';
-        var p = new RegExp(a.split('').join('|'), 'g');
+    enhanceWithButton() {
+        // Support the case that the placeholder may already be a button
+        if (this.placeholder.nodeName && this.placeholder.nodeName.toLowerCase() === 'button') {
+            this.trigger = this.placeholder;
+        } else {
+            this.trigger = document.createElement('button');
+            const placeholderAttributes = getAttributes(this.placeholder);
 
-        return string.toString().toLowerCase()
-            .replace(/\s+/g, '-') // Replace spaces with -
-            .replace(p, function(c) { return b.charAt(a.indexOf(c)); }) // Replace special characters
-            .replace(/&/g, '-') // Replace & with '-'
-            .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-            .replace(/\-\-+/g, '-') // Replace multiple - with single -
-            .replace(/^-+/, '') // Trim - from start of text
-            .replace(/-+$/, '') // Trim - from end of text
+            for (const entry in placeholderAttributes) {
+                this.trigger.setAttribute(entry, placeholderAttributes[entry]);
+            }
+            removeAllAttributes(this.placeholder);
+
+            this.root.prepend(this.trigger);
+            this.trigger.prepend(this.placeholder);
+        }
+
+        this.trigger.setAttribute('type', 'button');
     }
 
-    /**
-     * Check if Accordion is targeted by URL hash
-     *
-     * @param {jQuery} $trigger
-     * @returns {boolean}
-     */
-    function triggerIdMatchesUrlHash($trigger) {
-        return $trigger.attr('id') === getUrlHash();
-    }
+    events() {
 
-    /**
-     * Create a counter (used a for creation of unique IDs)
-     *
-     * @type {number}
-     */
-    window.wfaccordion = window.wfaccordion || {};
-    window.wfaccordion.slugs = window.wfaccordion.slugs || [];
+        // Update ARIA states on click/tap
+        this.trigger.addEventListener('click', (event) => {
+            const target = event.target.closest('button');
+            const state = target.getAttribute('aria-expanded') === 'false';
 
-    // constructor
-    var Accordion = function(elem, options) {
-        this.$elem = $(elem);
-        this.options = options;
-    };
-
-    // the plugin prototype
-    Accordion.prototype = {
-        defaults: {
-            accordionGroup: '.js-accordion-group',
-            accordionRoot: '.js-accordion',
-            accordionHeader: '.js-accordion__header',
-            accordionTrigger: '.js-accordion__trigger',
-            accordionPanel: '.js-accordion__panel',
-            disableHashUpdate: false
-        },
-
-        init: function() {
-            // Introduce defaults that can be extended either
-            // globally or using an object literal.
-            this.settings = $.extend({}, this.defaults, this.options);
-
-            var $accordionRoot = this.$elem.find(this.settings.accordionRoot);
-            var $accordionHeaders = this.$elem.find(this.settings.accordionHeader);
-            var self = this;
-
-            $accordionRoot.each(function(index, root) {
-                var $root = $(root);
-                var $header = $root.children($accordionHeaders);
-                var $triggerPlaceholder = $header.find(self.settings.accordionTrigger);
-                var $trigger = self.enhanceWithButton($triggerPlaceholder);
-                var $panel = $root.children(self.settings.accordionPanel);
-
-                // Create unique IDs for use in ARIA relationships
-                var headerId, panelId;
-                var accordionSlug = $root.attr('id') || slugify($trigger.text());
-                var slugOccurence = $.grep(window.wfaccordion.slugs, function (slug, index) {
-                    return slug.indexOf(accordionSlug) >= 0
-                }).length;
-
-                if ($.inArray(accordionSlug, window.wfaccordion.slugs) === -1) {
-                    window.wfaccordion.slugs.push(accordionSlug);
-                    headerId = accordionSlug;
-                    panelId = accordionSlug + '-panel';
-                } else {
-                    window.wfaccordion.slugs.push(accordionSlug + '-' + slugOccurence);
-                    headerId = accordionSlug + '-' + slugOccurence;
-                    panelId = accordionSlug + '-panel' + '-' + slugOccurence;
-                }
-
-                $root.removeAttr('id'); // Prevent duplicated IDs on root and header element
-
-                // Create ARIA relationships between headers and panels
-                $trigger.attr('aria-controls', panelId);
-                $panel.attr('id', panelId);
-
-                $trigger.attr('id', headerId);
-                $panel.attr('aria-labelledby', headerId);
-
-                var isDisabled = typeof $root.attr('data-wf-accordion-disabled') !== 'undefined';
-                var isTargetOfUrlHash = triggerIdMatchesUrlHash($trigger);
-                var isExpandedOnStartup = typeof $root.attr('data-wf-accordion-expanded') !== 'undefined' || isTargetOfUrlHash;
-
-                // Enhance triggers and panels with ARIA attributes
-                $trigger.attr('aria-expanded', false);
-                $panel.attr('aria-hidden', true);
-
-                // Handle initial accordion states
-                $trigger.attr('aria-disabled', isDisabled);
-                $trigger.attr('aria-expanded', isExpandedOnStartup);
-                $panel.attr('aria-hidden', !isExpandedOnStartup);
-
-                // Update ARIA states on click/tap
-                $trigger.on('click', function(event) {
-                    var $target = $(event.target).closest('button');
-                    var state = $target.attr('aria-expanded') === 'false';
-                    var id = $target.attr('id');
-
-                    if (!isDisabled) {
-                        $target.attr('aria-expanded', state);
-                        $('#' + $target.attr('aria-controls')).attr('aria-hidden', !state);
-                    }
-
-                    // Update URL with hash when an accordion expands
-                    if ($target.attr('aria-expanded') === 'true' && id !== getUrlHash() && !self.settings.disableHashUpdate) {
-                        setUrlHash(id);
-                    } else if ($target.attr('aria-expanded') === 'false') {
-                        removeUrlHash();
-                    }
-                });
-
-                // Enable keyboard support
-                $trigger.on('keydown', function(event) {
-                    var $target = $(event.target);
-                    var key = event.which.toString();
-                    var $header = $target.parents(self.settings.accordionHeader);
-
-                    // 33 = Page Up, 34 = Page Down
-                    var ctrlModifier = (event.ctrlKey && key.match(/33|34/));
-
-                    // Support up / down arrows as well as Ctrl + Page Up / Page Down keys
-                    // 38 = Up, 40 = Down
-                    if (key.match(/38|40/) || ctrlModifier) {
-                        var index = $accordionHeaders.index($header);
-                        var direction = (key.match(/34|40/)) ? 1 : -1;
-                        var length = $accordionHeaders.length;
-                        var newIndex = (index + length + direction) % length;
-
-                        $($accordionHeaders[newIndex]).find(self.settings.accordionTrigger).focus();
-
-                        event.preventDefault();
-                    } else if (key.match(/35|36/)) {
-                        // Support End and Home keys
-                        // 35 = End, 36 = Home
-                        switch (key) {
-                            // Focus the first accordion
-                            case '36':
-                                $($accordionHeaders[0]).find(self.settings.accordionTrigger).focus();
-                                break;
-                            // Focus the last accordion
-                            case '35':
-                                $($accordionHeaders[$accordionHeaders.length - 1]).find(self.settings.accordionTrigger).focus();
-                                break;
-                        }
-
-                        event.preventDefault();
-                    }
-                });
-
-                if (isTargetOfUrlHash) {
-                    $trigger.focus();
-                }
-            });
-
-            return this;
-        },
-
-        /**
-         * Enhance a given placeholder element with a <button> for better keyboard support
-         *
-         * @param {jQuery} $placeholder
-         * @returns {jQuery}
-         */
-        enhanceWithButton: function($placeholder) {
-            // Support the case that the placeholder may already be a button
-            if ($placeholder.get('0').nodeName && $placeholder.get('0').nodeName.toLowerCase() === 'button') {
-                return $placeholder;
+            if (!this.isDisabled) {
+                target.setAttribute('aria-expanded', state);
+                this.root.querySelector('#' + target.getAttribute('aria-controls')).setAttribute('aria-hidden', !state)
             }
 
-            var $button = $('<button>' + $placeholder.html() + '</button>');
-
-            $.each(this.getAttributes($placeholder), function(key, value) {
-                $button.attr(key, value);
-            });
-
-            // Ensure that the button is of type "button" to mitigate form submits in case the accordion is nested
-            // inside a form
-            $button.attr('type', 'button');
-
-            $placeholder.html($button);
-
-            this.removeAllAttributes($placeholder);
-
-            return $button;
-        },
-
-        /**
-         * Return all HTML attributes (and their values) of a given jQuery object
-         * @param {jQuery} $node
-         * @returns {{}}
-         */
-        getAttributes: function($node) {
-            var attrs = {};
-            $.each( $node[0].attributes, function (index, attribute) {
-                attrs[attribute.name] = attribute.value;
-            } );
-
-            return attrs;
-        },
-
-        /**
-         * Strip all HTML attributes (and their values) from a given jQuery object
-         * @param {jQuery} $node
-         */
-        removeAllAttributes: function($node) {
-            // Support non-jQuery node objects, just in case…
-            var elem = $node instanceof jQuery ? $node.get('0') : $node;
-
-            while(elem.attributes.length > 0) {
-                elem.removeAttribute(elem.attributes[0].name);
-            }
-        }
-    };
-
-    Accordion.defaults = Accordion.prototype.defaults;
-
-    $.fn.wfAccordion = function(options) {
-        var accordionGroupCount = this.length;
-
-        return this.each(function(index) {
-            new Accordion(this, options).init();
-
-            // Throw event when the last accordion group was processed (and DOM manipulations are done)
-            // Use vanilla JS events because "jQuery can catch vanilla JS events, but vanilla JS cannot catch jQuery
-            // added events."
-            if (index === accordionGroupCount - 1) {
-                var ALL_ACCORDIONS_PROCESSED = document.createEvent('Event');
-
-                ALL_ACCORDIONS_PROCESSED.initEvent('wf.accordions.mounted', true, true);
-                window.dispatchEvent(ALL_ACCORDIONS_PROCESSED);
+            // Update URL with hash when an accordion expands
+            if (!this.settings.disableHashUpdate) {
+                if (target.getAttribute('aria-expanded') === 'true' && this.headerID !== getUrlHash()) {
+                    setUrlHash(this.headerID);
+                } else if (target.getAttribute('aria-expanded') === 'false' && this.headerID === getUrlHash()) {
+                    removeUrlHash();
+                }
             }
         });
-    };
+    }
+}
 
-})(jQuery, window , document);
+class wfaccordionGroup {
+    constructor(elem, options) {
+        this.group = elem;
+        this.settings = {
+            ...{
+                accordionGroup: '.js-accordion-group',
+                accordionRoot: '.js-accordion',
+                disableHashUpdate: false
+            },
+            ...options
+        };
+        this.accordions = Array.from(this.group.querySelectorAll(this.settings.accordionRoot)).map((root) => {
+            return new wfaccordion(root);
+        });
+        this.accordionTrigger = this.accordions.map((accordion) => {
+            return accordion.trigger
+        });
+        this.events();
+    }
+
+    events() {
+
+        // Enable keyboard support
+        this.group.addEventListener('keydown', (event) => {
+            const target = event.target;
+            const key = event.which.toString();
+
+            // 33 = Page Up, 34 = Page Down
+            const ctrlModifier = (event.ctrlKey && key.match(/33|34/));
+
+            // Support up / down arrows as well as Ctrl + Page Up / Page Down keys
+            // 38 = Up, 40 = Down
+            if (key.match(/38|40/) || ctrlModifier) {
+                const index = this.accordionTrigger.indexOf(target);
+                const direction = (key.match(/34|40/)) ? 1 : -1;
+                const length = this.accordionTrigger.length;
+                const newIndex = (index + length + direction) % length;
+
+                this.accordionTrigger[newIndex].focus();
+
+                event.preventDefault();
+            } else if (key.match(/35|36/)) {
+                // Support End and Home keys
+                // 35 = End, 36 = Home
+                switch (key) {
+                    // Focus the first accordion
+                    case '36':
+                        this.accordionTrigger[0].focus();
+                        break;
+                    // Focus the last accordion
+                    case '35':
+                        this.accordionTrigger[this.accordionTrigger.length - 1].focus();
+                        break;
+                }
+                event.preventDefault();
+            }
+        });
+    }
+}
+
+const wfaccordionsInit = (options, initCallback) => {
+    const wfaccordionGroups = Array.from(document.querySelectorAll('.js-accordion-group'));
+
+    window.addEventListener('wf.accordions.mounted', (event) => {
+        if (initCallback) {
+            initCallback(event);
+        }
+    });
+
+    wfaccordionGroups.forEach((group, index) => {
+        window.wfaccordion.groups.push(new wfaccordionGroup(group, options || {}));
+
+        // Throw event when the last accordion group was processed (and DOM manipulations are done)
+        if (index === wfaccordionGroups.length - 1) {
+            window.dispatchEvent(new CustomEvent('wf.accordions.mounted', {
+                bubbles: true,
+                cancelable: true,
+                detail: window.wfaccordion
+            }));
+        }
+    });
+}
