582c2a3d8fceff92e9667a938fd4e7bd6793c8e8
Søren Birkemeyer <sb@webfactory.de>
2026-02-13 14:50:02 +0000

    Relax strict HTML structure requirement for accordion groups
    Replace strict parentElement check with `.closest(accordionGroup)` to support arbitrary wrapper elements between group and accordions while still excluding nested accordions. Add test with additional wrappers.
    
    This fixes an undocumented breaking change that was introduced in v5.0.0.

--- src/accordion.js
+++ src/accordion.js
@@ -133,7 +133,7 @@ class wfaccordionGroup {
             ...options
         };
         this.accordions = Array.from(this.group.querySelectorAll(this.settings.accordionRoot))
-            .filter(root => this.group === root.parentElement)
+            .filter(root => this.group === root.closest(this.settings.accordionGroup))
             .map((root) => {
                 return new wfaccordion(root, options);
             });
